6b7f6a9c735ccf938f20cf242ea778961f3a015c
Thiago Corgosinho <81438506+corgosinn@users.noreply.github.com>
2022-06-07 12:22:56 -0300

    commit 2.0

--- grafo.py
+++ grafo.py
@@ -1,100 +1,182 @@
 from asyncio.windows_events import NULL
 from dis import dis
+import enum
+from operator import le
 
 
 class Grafo:
 
-    def __init__(self, num_vert=0, num_arestas=0, lista_adj=None, mat_adj=None):
-        self.num_vert = num_vert
-        self.num_arestas = num_arestas
-        if lista_adj is None:
-            self.lista_adj = [[] for i in range(num_vert)]
-        else:
-            self.lista_adj = lista_adj
-        if mat_adj is None:
-            self.mat_adj = [[0 for j in range(num_vert)]
-                            for i in range(num_vert)]
-        else:
-            self.mat_adj = mat_adj
+    def __init__(self, num_vert=0, num_arestas=0):
+        self.quantidade_vertices = num_vert
+        self.quantidade_arestas = num_arestas
+        self.nome = ['' for i in range(num_vert)]
+        self.mat_adj = [[0 for j in range(num_vert)]
+                        for i in range(num_vert)]
+        self.w_adj = [[0 for j in range(num_vert)]  # Peso separado da matriz original
+                      for i in range(num_vert)]
+        self.c_adj = [[0 for j in range(num_vert)]  # Matriz de c
+                      for i in range(num_vert)]
+        self.numeroAresta = [[0 for j in range(num_vert)]  # Cada aresta tera um numero
+                             for i in range(num_vert)]
+        self.b = [0 for i in range(num_vert)]  # Matriz de b
+        self.lista_adj = []  # Lista de adj
+        self.contagem = 0  # Inicio dos vertices das disciplinas
+        self.nomeCompleto = ['' for i in range(num_vert)]
 
-    def add_aresta(self, u, v, w=1):
-        """Adiciona aresta de u a v com peso w"""
-        self.num_arestas += 1
-        if u < self.num_vert and v < self.num_vert:
-            self.lista_adj[u].append((v, w))
-            self.mat_adj[u][v] = w
+    def addAresta(self, u, v, w=1, c=0, op=0):
+        if u < self.quantidade_vertices and v < self.quantidade_vertices:
+            self.numeroAresta[u][v] = self.quantidade_arestas
+            self.quantidade_arestas += 1
+            self.mat_adj[u][v] = 1
+            self.w_adj[u][v] = w
+            self.c_adj[u][v] = c
+            self.lista_adj.append((u, v, w))
+
         else:
             print("Aresta invalida!")
 
-    def remove_aresta(self, u, v):
-        """Remove aresta de u a v, se houver"""
-        if u < self.num_vert and v < self.num_vert:
-            if self.mat_adj[u][v] != 0:
-                self.num_arestas += 1
-                self.mat_adj[u][v] = 0
-                for (v2, w2) in self.lista_adj[u]:
-                    if v2 == v:
-                        self.lista_adj[u].remove((v2, w2))
-                        break
-            else:
-                print("Aresta inexistente!")
-        else:
-            print("Aresta invalida!")
-
-    def ler_arquivo(self, nome_arq):
+    def ler_arquivo(self, csvProf, csvDisc, b):
         """Le arquivo de grafo no formato dimacs"""
         try:
-            arq = open(nome_arq)
-            # Leitura do cabecalho
-            str = arq.readline()
-            str = str.split(" ")
-            self.num_vert = int(str[0])
-            cont_arestas = int(str[1])
-            # Inicializacao das estruturas de dados
-            self.lista_adj = [[] for i in range(self.num_vert)]
-            self.mat_adj = [[0 for j in range(self.num_vert)]
-                            for i in range(self.num_vert)]
-            # Le cada aresta do arquivo
-            for i in range(0, cont_arestas):
-                str = arq.readline()
-                str = str.split(" ")
-                u = int(str[0])  # Vertice origem
-                v = int(str[1])  # Vertice destino
-                w = int(str[2])  # Peso da aresta
-                self.add_aresta(u, v, w)
+            # Leitura csv
+            self.quantidade_arestas = 0
+            self.quantidade_vertices = 1
+            dicPrioridades = {0: 0, 1: 3, 2: 5, 3: 8, 4: 10}
+            dicProfs = []
+            for elem in csvProf:
+                if(int(elem[1]) > 0 and len(elem[0]) > 0):
+                    self.quantidade_vertices = self.quantidade_vertices + 1
+            for elem in csvDisc:
+                if len(elem[0]) > 0:
+                    self.quantidade_vertices = self.quantidade_vertices + 1
+            self.lista_adj = []
+            self.mat_adj = [[0 for j in range(self.quantidade_vertices)]
+                            for i in range(self.quantidade_vertices)]
+            self.c_adj = [[0 for j in range(self.quantidade_vertices)]
+                          for i in range(self.quantidade_vertices)]
+            self.w_adj = [[0 for j in range(self.quantidade_vertices)]
+                          for i in range(self.quantidade_vertices)]
+            self.numeroAresta = [[0 for j in range(self.quantidade_vertices)]
+                                 for i in range(self.quantidade_vertices)]
+            self.nome = ['' for i in range(self.quantidade_vertices)]
+            self.nomeCompleto = ['' for i in range(self.quantidade_vertices)]
+            self.b = [0 for i in range(self.quantidade_vertices)]
+
+            self.nome[0] = 'S'
+            self.nome[self.quantidade_vertices-1] = 'R'
+            indiceDisciplinas = 1
+            self.b[0] = b
+            self.b[self.quantidade_vertices-1] = -(b)
+            for indice, elem in enumerate(csvProf):
+                if(int(elem[1]) > 0 and len(elem[0]) > 0):
+                    self.nome[indiceDisciplinas] = elem[0]
+                    indiceDisciplinas = indiceDisciplinas + 1
+                    self.addAresta(0, indice+1, int(elem[1]), elem[1])
+            self.contagem = indiceDisciplinas
+            for indice, elem in enumerate(csvDisc):
+                if len(elem[0]) > 0:
+                    self.nome[indiceDisciplinas] = elem[0]
+                    self.nomeCompleto[indiceDisciplinas] = elem[1]
+                    indiceDisciplinas = indiceDisciplinas + 1
+            for indice, elem in enumerate(csvProf):
+                dicProfs.clear()
+                dicProfs.append(elem[2])
+                dicProfs.append(elem[3])
+                dicProfs.append(elem[4])
+                dicProfs.append(elem[5])
+                dicProfs.append(elem[6])
+                for materia in range(1, self.quantidade_vertices):
+                    if self.nome[materia] in dicProfs and self.nome[materia] != '':
+                        self.addAresta(
+                            indice+1, materia, dicPrioridades[dicProfs.index(self.nome[materia])], 9999)
+            for indice, elem in enumerate(csvDisc):
+                if len(elem[0]) > 0:
+                    self.addAresta(self.nome.index(
+                        elem[0]), self.quantidade_vertices-1, 0, elem[2])
+
         except IOError:
             print("Nao foi possivel encontrar ou ler o arquivo!")
 
-    def BELLMAN_FORD(self, s, destino):
-        dist = [float('inf') for v in range(self.num_vert)]
-        pred = [NULL for v in range(self.num_vert)]
+    def remove_aresta(self, u, v):
+
+        if u < self.quantidade_vertices and v <= self.quantidade_vertices:
+            if self.mat_adj[u][v] != 0:
+                self.quantidade_arestas -= 1
+                self.mat_adj[u][v] = 0
+                self.c_adj[u][v] = 0
+                self.lista_adj.remove((u, v, self.w_adj[u][v]))
+                self.w_adj[u][v] = 0
+            else:
+                print("Aresta inexistente!")
+                return -1
+        else:
+            print("Aresta invalida!")
+
+    def bellman_ford(self, s=0):
+        dist = [float("inf") for _ in range(
+            self.quantidade_vertices)]  # Distance from s
+        # Predecessor in shortest path from s
+        pred = [None for _ in range(self.quantidade_vertices)]
         dist[s] = 0
-        for j in range(self.num_vert - 1):
-            mudou = 0
-            for u in range(len(self.lista_adj)):
-                for (v1, v2) in self.lista_adj[u]:
-                    mudou = 1
-                    if dist[v1] > dist[u] + v2:
-                        dist[v1] = dist[u] + v2
-                        pred[v1] = u
-            if mudou == 0:
+        for it in range(self.quantidade_vertices):
+            updated = False
+            for (u, v, w) in self.lista_adj:
+                if dist[v] > dist[u] + w:
+                    dist[v] = dist[u] + w
+                    pred[v] = u
+                    updated = True
+            if not updated:
                 break
+        path = []
+        if pred[len(pred) - 1] != None:
+            n = pred[len(pred) - 1]
+            path.append(self.quantidade_vertices-1)
+            while(True):
+                path.append(n)
+                if(n == 0):
+                    break
+                n = pred[n]
+            path.reverse()
+            min = 9999999
+            for x in range(len(path)):
+                if x != len(path)-1:
+                    if int(self.c_adj[path[x]][path[x+1]]) < int(min):
+                        min = self.c_adj[path[x]][path[x+1]]
+            return pred[self.quantidade_vertices - 1], path, min
+        else:
+            return None, None, 0
 
-        caminho = self.print_caminho(pred, s, destino)
-        self.soma_custo(s, destino, dist, caminho)
-        return dist
+    def SCM(self):
+        F = [0 for i in range(self.quantidade_arestas*2)]
+        CUSTO = [0 for i in range(self.quantidade_arestas*2)]
+        listaBellmanFord = self.bellman_ford()
 
-    def soma_custo(self, origem, destino, dist, caminho, busca=0):
-        print(f"Custo: {dist[destino]}")
+        while(listaBellmanFord[0] != None and self.b[0] > 0):
 
-    def print_caminho(self, pred, origem, destino):
-        x = destino
-        caminho = []
-        while(x != origem):
-            caminho.append(x)
-            x = pred[x]
-        caminho.append(origem)
-        caminho.reverse()
+            C = listaBellmanFord[1]
+            f = int(listaBellmanFord[2])
+            for elementosEmC in range(len(C)):
 
-        print(f"Caminho: {caminho}")
-        return caminho
+                if elementosEmC != len(C)-1:
+                    u = C[elementosEmC]
+                    v = C[elementosEmC+1]
+                    F[self.numeroAresta[u][v]] = F[self.numeroAresta[u][v]] + f
+                    CUSTO[self.numeroAresta[u][v]
+                          ] = CUSTO[self.numeroAresta[u][v]] + self.w_adj[u][v]
+                    self.c_adj[u][v] = int(self.c_adj[u][v]) - f
+
+                    if self.c_adj[u][v] == 0:
+                        if(self.remove_aresta(u, v) == -1):
+                            return
+
+                    if self.mat_adj[v][u] == 0:
+                        self.addAresta(v, u, -(self.w_adj[u][v]), 999, 1)
+                    self.c_adj[v][u] = self.c_adj[v][u] + f
+                    if F[self.numeroAresta[v][u]] != 0:
+                        F[self.numeroAresta[v][u]] = F[self.numeroAresta[v][u]] - f
+
+            self.b[0] = self.b[0] - f
+            self.b[self.quantidade_vertices -
+                   1] = self.b[self.quantidade_vertices-1] + f
+            listaBellmanFord = self.bellman_ford()
+        return F, CUSTO
--- trabalho.py
+++ trabalho.py
@@ -1,6 +1,90 @@
-from colorsys import rgb_to_yiq
-from time import time
+# Thiago Corgosinho Silva - 20.2.8117
+from prettytable import PrettyTable
 import grafo
 import time
+import os
+import csv
 
-# Thiago Corgosinho Silva - 20.2.8117
+# LER ARQUIVOS CSV
+disciplinaCSV = []
+professoresCSV = []
+
+# MENU DE ARQUIVOS
+fileDisciplina = 'disciplinas_toy.csv'
+fileProfessores = 'professores_toy.csv'
+print("Escolha um arquivo de disciplina: ")
+print("1- discplinas_toy.csv")
+print("2- disciplinas.csv")
+print('3- Outro')
+
+op = int(input("Digite: "))
+
+if op == 1:
+    fileDisciplina = 'disciplinas_toy.csv'
+elif op == 2:
+    fileDisciplina = 'disciplinas.csv'
+else:
+    print("Digite o nome do arquivo: ")
+    fileDisciplina = input()
+print("Escolha um arquivo de professores: ")
+print("1- professores_toy.csv")
+print("2- professores.csv")
+print('3- Outro')
+
+op = int(input("Digite: "))
+
+if op == 1:
+    fileProfessores = 'professores_toy.csv'
+elif op == 2:
+    fileProfessores = 'professores.csv'
+else:
+    print("Digite o nome do arquivo: ")
+    fileProfessores = input()
+
+
+somaFluxo = 0
+
+# LER DISCIPLINAS CSV
+try:
+    fileDisciplinaCSV = open(os.path.join(os.path.dirname(__file__),
+                                          f'csv\\{fileDisciplina}'), encoding='utf-8', newline='')
+    csvReader = csv.reader(fileDisciplinaCSV, delimiter=';')
+    for x, dadoCsv in enumerate(csvReader):
+        if(x != 0):
+            disciplinaCSV.append(dadoCsv)
+    fileDisciplinaCSV.close()
+except:
+    print("Erro ao ler disciplinas")
+
+# LER PROFESSORES CSV
+
+try:
+    fileProfessoresCSV = open(os.path.join(os.path.dirname(__file__),
+                                           f'csv\\{fileProfessores}'), encoding='utf-8', newline='')
+    csvProfReader = csv.reader(fileProfessoresCSV, delimiter=';')
+    for x, dadoCsv in enumerate(csvProfReader):
+        if(x != 0):
+            professoresCSV.append(dadoCsv)
+    fileDisciplinaCSV.close()
+except:
+    print("Erro ao ler professores")
+
+# DEFINIR GRAFOS
+
+for x in range(0, len(professoresCSV)):
+    somaFluxo = somaFluxo + int(professoresCSV[x][1])
+grafo = grafo.Grafo()
+grafo.ler_arquivo(professoresCSV, disciplinaCSV, somaFluxo)
+
+dados = grafo.SCM()
+qtdTurmas = dados[0]
+
+
+# PRINTA TABELA NA TELA
+table = PrettyTable(['Professor', 'Disciplina', 'Nome', 'Turmas', 'Custo'])
+for linha in range(grafo.contagem):
+    for coluna in range(len(grafo.mat_adj[linha])):
+        if(grafo.mat_adj[linha][coluna] == 1 and qtdTurmas[grafo.numeroAresta[linha][coluna]] > 0 and grafo.nome[coluna] != 'S'):
+            table.add_row([grafo.nome[linha], grafo.nome[coluna], grafo.nomeCompleto[coluna],
+                           qtdTurmas[grafo.numeroAresta[linha][coluna]], dados[1][grafo.numeroAresta[linha][coluna]]])
+print(table)
